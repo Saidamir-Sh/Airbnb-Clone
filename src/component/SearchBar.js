57cfd82698a4c627c3389a3cdf08431d41908c79
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-03 19:00:35 +0500

    SearchBar structure added

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -5,17 +5,28 @@ import { Form } from 'react-bootstrap'
 function SearchBar() {
     return (
         <div className='SearchBar'>
-            <Container>
+            <Container className="container-100">
                 <Row>
                     <Form>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                          <Form.Label>Email address</Form.Label>
-                          <Form.Control type="email" placeholder="name@example.com" />
+                        <Form.Group className="mb-3" controlId="location">
+                          <Form.Label>Location</Form.Label>
+                          <Form.Control id='location' type="text" placeholder="Where are you going ?" style={{}}/>
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                          <Form.Label>Example textarea</Form.Label>
-                          <Form.Control as="textarea" rows={3} />
+                        <Form.Group className="mb-3" controlId="date">
+                          <Form.Label>Check in</Form.Label>
+                          <Form.Control id='date' type='date' placeholder='Add dates'/>
                         </Form.Group>
+                        <Form.Group className="mb-3" controlId="date">
+                          <Form.Label>Check out</Form.Label>
+                          <Form.Control id='date' type='date' placeholder='Add dates'/>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="date">
+                          <Form.Label>Guests</Form.Label>
+                          <Form.Control id='date' type='text' placeholder='Add guests'/>
+                        </Form.Group>
+                        <div>
+                            <i class="bi bi-search"></i>
+                        </div>
                     </Form>
                 </Row>
             </Container>
