78016c3ec76a0870efa79a103e0c39f080372b81
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-03 17:53:17 +0500

    added SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './component/Navbar';
+import SearchBar from './component/SearchBar';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <SearchBar />
     </div>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,7 +6,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 function Navbar() {
     return (
-        <Container fluid={true} style={{backgroundColor: '#000'}}>
+        <Container fluid={true} style={{backgroundColor: '#000', fontSize: '.9rem', fontWeight: 'bold'}}>
             <Row className='d-flex justify-content-between align-items-center text-white'>
                 <Col md={2}>
                     <img src={logo} style={{width: '6rem'}}/>
@@ -16,11 +16,11 @@ function Navbar() {
                     <p>Experiences</p>
                     <p>Online Experiences</p>
                 </Col>
-                <Col md={2} className='d-flex justify-content-between'>
-                    <p>Become a Host</p>
+                <Col md={2} className='d-flex justify-content-between align-items-center'>
+                    <p className='mt-3 mp-5'>Become a Host</p>
                     <i class="bi bi-globe" />
-                    <div className='d-flex align-items-center justfy-content-center' style={{width: '5rem', border: '1px solid white', borderRadius: '15px'}}>
-                        <i class="bi bi-list"></i>
+                    <div className='d-flex justify-content-around align-items-center bg-white' style={{width: '4rem', border: '1px solid white', borderRadius: '15px'}}>
+                        <i class="bi bi-list" style={{color: '#000'}}></i>
                         <img src={user} style={{width: '2rem', borderRadius: '50%'}}/>
                     </div>
                 </Col>
