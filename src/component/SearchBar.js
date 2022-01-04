0de7148e4a615002c0b9773d4a69ab4b5c105956
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-04 13:21:22 +0500

    SearchBar finished

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,31 +1,45 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import '../styles/SearchBar.css'
 import { Container, Row } from 'react-bootstrap'
 import { Form } from 'react-bootstrap'
 
 function SearchBar() {
+  const ref = useRef()
     return (
         <div className='SearchBar'>
-            <Container className="container-100">
-                <Row>
-                    <Form>
+            <Container fluid={true} style={{backgroundColor: '#000'}}>
+                <Row className='d-flex justify-content-center'>
+                    <Form  className='d-flex justify-content-between align-items-center bg-white my-3 px-4' style={{borderRadius: '30px'}}>
                         <Form.Group className="mb-3" controlId="location">
-                          <Form.Label>Location</Form.Label>
-                          <Form.Control id='location' type="text" placeholder="Where are you going ?" style={{}}/>
+                          <Form.Label className='bg-white w-100 font-wight-bold'>Location</Form.Label>
+                          <Form.Control id='location' type="text" placeholder="Where are you going ?" style={{border: 'none'}}/>
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="date">
+                        <Form.Group className="mb-3" controlId="date-in">
                           <Form.Label>Check in</Form.Label>
-                          <Form.Control id='date' type='date' placeholder='Add dates'/>
+                          <Form.Control 
+                            id='date-in' 
+                            type='text'  
+                            placeholder='Add dates'
+                            ref={ref}
+                            onFocus={() => {ref.current.type = "date"}}
+                            onBlur={() => (ref.current.type = "text")}/>
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="date">
+                        <Form.Group className="mb-3" controlId="date-out">
                           <Form.Label>Check out</Form.Label>
-                          <Form.Control id='date' type='date' placeholder='Add dates'/>
+                          <Form.Control 
+                            id='date-out' 
+                            type='text' 
+                            placeholder='Add dates'
+                            ref={ref}
+                            onFocus={() => {ref.current.type = "date"}}
+                            onBlur={() => (ref.current.type = "text")}/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="date">
                           <Form.Label>Guests</Form.Label>
-                          <Form.Control id='date' type='text' placeholder='Add guests'/>
+                          <Form.Control id='guests' type='number' placeholder='Add guests'/>
                         </Form.Group>
-                        <div>
-                            <i class="bi bi-search"></i>
+                        <div className='d-flex align-items-center justify-content-center' style={{width: '3rem', height: '3rem', borderRadius: '50%', backgroundColor: 'DarkSalmon'}}>
+                            <i class="bi bi-search" style={{color: '#fff'}}></i>
                         </div>
                     </Form>
                 </Row>
