f18c53498bf9763be186a1b4cec05bed6b43a72e
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-05 15:51:52 +0500

    footer starter template

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SearchBar from './component/SearchBar';
 import HomePage from './component/HomePage';
 import Inspiration from './component/Inspiration';
 import Discover from './component/Discover';
+import Footer from './component/Footer'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
      <HomePage />
      <Inspiration />
      <Discover />
+     <Footer />
     </div>
   );
 }
