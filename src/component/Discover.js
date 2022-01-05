98dbb1c6dd6f5b77c85a4a0d95fcedb3fa813323
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-05 15:47:01 +0500

    discover page cards

--- src/component/Discover.js
+++ src/component/Discover.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import sea from '../assets/sea.jpg'
-import seaskate from '../assets/seaskate.jpg'
-import { Container, Row, Col } from 'react-bootstrap'
+import forest from '../assets/forest.jpg'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
 function Discover() {
     return (
@@ -9,11 +9,19 @@ function Discover() {
             <Container>
                 <h1>Discover Airbnb Experiences</h1>
                 <Row>
-                    <Col md={6} xs={12}>
+                    <Col md={6} xs={12} className='position-relative'>
                         <img src={sea} width={'100%'}/>
+                        <div className='position-absolute' style={{width: '50%',top: '2rem', left: '2em'}}>
+                            <h2>Things to do on your trip</h2>
+                            <Button variant='light'>Experiences</Button>
+                        </div>
                     </Col>
                     <Col md={6} xs={12}>
-                        <img src={seaskate} width={'100%'} />
+                        <img src={forest} width={'100%'} />
+                        <div className='position-absolute' style={{width: '50%',top: '2rem', left: '2em'}}>
+                            <h2>Things to do from home</h2>
+                            <Button variant='light'>Experiences</Button>
+                        </div>
                     </Col>
                 </Row>
             </Container>
--- src/component/Inspiration.js
+++ src/component/Inspiration.js
@@ -3,10 +3,10 @@ import { Container, Row, Col, Card } from 'react-bootstrap'
 
 function Inspiration() {
     return (
-        <div className='Inspiration'>
+        <div className='Inspiration py-4'>
             <Container>
             <h1>Inspiration for your next trip</h1>
-                <Row className='d-flex justify-content-around'>
+                <Row className='d-flex justify-content-around py-3'>
                     <Col md={3}  xs={1} s={2}>
                         <Card>
                             <Card.Img variant="top" src="https://q-xx.bstatic.com/images/hotel/max1024x768/204/204676165.jpg" />
