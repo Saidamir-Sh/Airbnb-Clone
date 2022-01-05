b3fd73375e16c8ed8701bd453c181523f2305806
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-05 22:21:08 +0500

    footer link template

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
     return (
-        <div>
-            
+        <div className='Footer'>
+            <Container>
+                <Row>
+                    <Col>
+                        <ul>
+                            <li>Support</li>
+                            <li>Help center</li>
+                            <li>Safety Information</li>
+                            <li>Cancellation options</li>
+                            <li>Our Covid-19 Response</li>
+                        </ul>
+                    </Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+                <Row></Row>
+            </Container>
         </div>
     )
 }
