b5697773159590ba7020749951abd12c253c773f
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-06 14:10:38 +0500

    navbar links restyled

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import user from '../assets/user.png'
+import '../styles/Navbar.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Container, Row, Col } from 'react-bootstrap'
 
 function Navbar() {
     return (
-        <Container fluid={true} style={{backgroundColor: '#000', fontSize: '1rem', fontWeight: 'light'}}>
-            <Row className='d-flex justify-content-between align-items-center text-white' style={{width: '90%', margin: '0 auto'}}>
+        <Container className='pt-2' fluid={true} style={{backgroundColor: '#000', fontSize: '1rem', fontWeight: 'light'}}>
+            <Row className='d-flex justify-content-between align-items-center text-white' style={{width: '93%', margin: '0 auto'}}>
                 <Col md={2}>
                     <img src={logo} style={{width: '6rem'}}/>
                 </Col>
-                <Col md={4} className='d-flex justify-content-between align-items-center'>
-                    <p>Place to stay</p>
-                    <p>Experiences</p>
-                    <p>Online Experiences</p>
+                <Col id='nav-links' md={4} className='d-flex justify-content-between align-items-center mt-3 ml-5'>
+                    <a href='#'>Places to stay</a>
+                    <a href='#'>Experiences</a>
+                    <a href='#'>Online Experiences</a>
                 </Col>
                 <Col md={3} className='d-flex justify-content-around align-items-center'>
                     <p className='mt-3'>Become a Host</p>
