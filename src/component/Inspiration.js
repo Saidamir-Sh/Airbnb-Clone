48ad5e3b469df0e7b1905164ffad6355f27a31ba
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-01-08 23:52:47 +0500

    inspiration page cards finished

--- src/component/Inspiration.js
+++ src/component/Inspiration.js
@@ -8,15 +8,15 @@ import { Container, Row, Col, Card } from 'react-bootstrap'
 
 function Inspiration() {
     return (
-        <div className='Inspiration py-4'>
+        <div className='Inspiration py-5'>
             <Container>
-            <h1>Inspiration for your next trip</h1>
+            <h1 className='font-weight-bold'>Inspiration for your next trip</h1>
                 <Row className='d-flex justify-content-around py-3'>
                     <Col md={3}  xs={1} s={2}>
-                        <Card>
+                        <Card style={{backgroundColor: '#CB2C49', borderRadius: '15px'}}>
                             <Card.Img  variant="top" src={sapanca} />
-                            <Card.Body className='pb-5'>
-                              <Card.Title>Sapanca</Card.Title>
+                            <Card.Body className='pb-5 text-white'>
+                              <Card.Title className='font-weight-bold'>Sapanca</Card.Title>
                               <Card.Text className='pb-5'>
                                 155 kilometers away
                               </Card.Text>
@@ -24,34 +24,34 @@ function Inspiration() {
                         </Card>
                     </Col>
                     <Col md={3}  xs={1} s={2}>
-                        <Card>
+                        <Card style={{backgroundColor: '#DC304F', borderRadius: '15px'}}>
                             <Card.Img variant="top" src={adalar} />
-                            <Card.Body className='pb-5'>
-                              <Card.Title>Sapanca</Card.Title>
+                            <Card.Body className='pb-5 text-white'>
+                              <Card.Title className='font-weight-bold'>Adalar</Card.Title>
                               <Card.Text className='pb-5'>
-                                155 kilometers away
+                                20 kilometers away
                               </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col md={3}  xs={1} s={2}>
-                        <Card>
+                        <Card style={{backgroundColor: '#BC186D', borderRadius: '15px'}}>
                             <Card.Img variant="top" src={bodrum} />
-                            <Card.Body className='pb-5'>
-                              <Card.Title>Sapanca</Card.Title>
+                            <Card.Body className='pb-5 text-white'>
+                              <Card.Title className='font-weight-bold'>Bodrum</Card.Title>
                               <Card.Text className='pb-5'>
-                                155 kilometers away
+                                462 kilometers away
                               </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col md={3}  xs={1} s={2}>
-                        <Card>
+                        <Card style={{backgroundColor: '#D83A2F', borderRadius: '15px'}}>
                             <Card.Img variant="top" src={bolu} />
-                            <Card.Body className='pb-5'>
-                              <Card.Title>Sapanca</Card.Title>
+                            <Card.Body className='pb-5 text-white'>
+                              <Card.Title className='font-weight-bold'>Bolu</Card.Title>
                               <Card.Text className='pb-5'>
-                                155 kilometers away
+                                225 kilometers away
                               </Card.Text>
                             </Card.Body>
                         </Card>
